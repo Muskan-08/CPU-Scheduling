f6ebd4327447bd78e02416c1a25ab097a48b0027
Muskan-08 <49245747+Muskan-08@users.noreply.github.com>
2019-04-05 20:32:47 +0530

    added priorities

--- m2.cpp
+++ m2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 	int n;
-	int burst_time[20];
+	int burst_time[20],arv_time[20],pr[20],temp[20];
 	cout<<"Enter the number of the process you want to add\t";
 	cin>>n;
 	cout<<"\nEnter the Burst time of the processes";
@@ -11,5 +11,16 @@ int main()
 	{
 		cout<<"\nEnter the Burst time of the P"<<i<<"\t";
 		cin>>burst_time[i];
+		cout<<"\nEnter the Arrival time of the P"<<i<<"\t";
+		cin>>arv_time[i];
 	}
+    for(int i=0;i<n;i++)
+    {
+      temp[i]=burst_time[i];  
+    }
+    burst_time.sort();
+    /*for(int i=0;i<n;i++)
+    {
+        
+    }*/
 }
