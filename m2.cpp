33d74f6d65509aad9cc99293efc0b756dad2bff5
Muskan-08 <49245747+Muskan-08@users.noreply.github.com>
2019-04-09 01:14:26 +0530

    Add files via upload

--- m2.cpp
+++ m2.cpp
@@ -2,7 +2,7 @@ using namespace std;
 #include<iostream>
 int main()
 {
-	int n;
+	int n,t,p;
 	int burst_time[20],arv_time[20],pr[20],temp[20];
 	cout<<"Enter the number of the process you want to add\t";
 	cin>>n;
@@ -14,13 +14,51 @@ int main()
 		cout<<"\nEnter the Arrival time of the P"<<i<<"\t";
 		cin>>arv_time[i];
 	}
-    for(int i=0;i<n;i++)
+	for(int i=0;i<n;i++)
     {
       temp[i]=burst_time[i];  
     }
-    burst_time.sort();
-    /*for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++)
     {
-        
-    }*/
+        for(int j=i+1;j<n;j++)
+        {
+        if(temp[i]>temp[j])
+        {
+        t=temp[i];
+        temp[i]=temp[j];
+        temp[j]=t;
+        }
+        }    
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<temp[i]<<"\t";
+    }
+    cout<<"\n";
+    for(int i=0;i<n;i++)
+    {
+        pr[i]=i;
+    }
+   
+    for(int i=0;i<n;i++)
+    {
+        cout<<pr[i]<<"\t";
+    }
+  for(int i=0;i<n;i++)
+  {
+      for(int j=0;j<n;j++)
+      {
+          if(temp[i]==burst_time[j])
+          {
+              p=pr[i];
+              pr[i]=pr[j];
+              pr[j]=p;
+          }
+      }
+  }
+  cout<<"\n";
+  for(int i=0;i<n;i++)
+    {
+        cout<<pr[i]<<"\t";
+    }
 }
